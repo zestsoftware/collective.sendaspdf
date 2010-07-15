59f92e1782353ee71496fd6544c97e0fb12a1830
Vincent Pretre <v.pretre@zestsoftware.nl>
2010-07-15 19:40:47 +0200

    added form to send the mail (which is send, still trouble with the message)

--- collective/sendaspdf/browser/send.py
+++ collective/sendaspdf/browser/send.py
@@ -3,6 +3,8 @@ from datetime import datetime
 
 import ho.pisa as pisa
 
+from AccessControl import Unauthorized
+from Products.validation import validation
 from Products.Five.browser import BrowserView
 from Products.CMFCore.utils import getToolByName
 from Products.Archetypes.config import RENAME_AFTER_CREATION_ATTEMPTS
@@ -11,23 +13,20 @@ from jquery.pyproxy.plone import jquery, JQueryProxy
 from jquery.pyproxy.base import clean_string
 
 from collective.sendaspdf.utils import md5_hash, extract_from_url
+from collective.sendaspdf.emailer import send_message
 
-class SendByMail(BrowserView):
-    """ If the user does not have Javascript enabled, he is
-    redirected to this page.
-    This page generates the PDF using the URL of the previous page
-    and shows a form to send it by mail.
-
-    It should logically show the same thing than the previous page
-    (the user has no JS enabled so no funky stuff happened).
-    The only case it could fail is if the previous page was processing a
-    POST form. In this case, we can just render the page as if it was
-    called with a simple GET request.
+class BaseView(BrowserView):
+    """ Class used to factorize some code for the different views
+    used in the product.
     """
+    error_mapping = {'email': ['email', 'invalid_email'],
+                     'email_recipient': ['email_recipient',
+                                         'invalid_email_recipient']}
+
     def __init__(self, *args, **kwargs):
         """ We just need to define some instance variables.
         """
-        super(SendByMail, self).__init__(*args, **kwargs)
+        super(BaseView, self).__init__(*args, **kwargs)
 
         # The list of errors found when checking the form.
         self.errors = []
@@ -37,46 +36,33 @@ class SendByMail(BrowserView):
         # from the ZMI.
         self.tempdir = '/tmp/' #XXX - use tempdir
         self.salt = 'kikoo'
-        self.filename_in_mail = 'screenshot'
+        self.filename_in_mail = 'screenshot.pdf'
 
         self.pdf_file = None
+        self.filename = ''
 
-    def get_page_source(self):
-        """ Returns the HTML source of a web page, considering
-        that the URL of the page is contained in the form under
-        the 'page_url' key.
-        """
-        url = self.request.form['page_url']
-        context_url = self.context.absolute_url()
-
-        view_name, get_params = extract_from_url(url, context_url)
-
-        # Now we will reinject the GET parameters in the request.
-        if get_params:
-            for key in get_params:
-                self.request.form[key] = get_params[key]
-
-        if not view_name:
-            default_view = self.context.defaultView()
-            if default_view in self.context:
-                view = self.context[default_view]
-        else:
-            try:
-                view = self.context.restrictedTraverse('@@' + view_name)
-            except:
-                view = self.context
-
-        return view()
-
-    def get_user_email(self):
-        """ Returns the currently logged-in user's email.
+    def get_user(self):
+        """ Returns the currently logged-in user.
         """
         mtool = getToolByName(self.context, 'portal_membership')
         if mtool.isAnonymousUser():
             return
 
-        member = mtool.getAuthenticatedUser()
-        return member.getProperty('email')
+        return mtool.getAuthenticatedMember()
+
+    def get_user_fullname(self):
+        """ Returns the currently logged-in user's fullname.
+        """
+        member = self.get_user()
+        if member:
+            return member.getProperty('fullname')
+
+    def get_user_email(self):
+        """ Returns the currently logged-in user's email.
+        """
+        member = self.get_user()
+        if member:
+            return member.getProperty('email')
 
     def generate_filename_prefix(self):
         """ Returns the user's email hashed in md5 followed
@@ -94,6 +80,110 @@ class SendByMail(BrowserView):
             return ''
 
         return '%s_' % md5_hash(email, self.salt)
+
+    def show_error_message(self, error_name):
+        """ Tells if an error message should be shown in the template.
+        """
+        return error_name in self.errors
+
+    def class_for_field(self, fieldname):
+        """ Returns the class that should be applied to a field
+        in the forms displayed by the product.
+        """
+        base_class = 'field'
+        error_class = ' error'
+        if not fieldname in self.error_mapping:
+            if fieldname in self.errors:
+                base_class += error_class
+            return base_class
+
+        for error_name in self.error_mapping[fieldname]:
+            if self.show_error_message(error_name):
+                return base_class + error_class
+        return base_class
+
+class RealURLView(BaseView):
+    """ We need this view to build the 'send as pdf'
+    action menu.    
+    """
+    def __call__(self):
+        base = self.context.REQUEST['ACTUAL_URL']
+        get_params = '&'.join(
+            ['%s=%s' % (k, v) for k, v in self.context.REQUEST.form.items()])
+
+        if get_params:
+           base += '?' + get_params
+        return base
+
+class DownloadPDF(BaseView):
+    """ View called when clicking the 'Click here to preview'
+    link.
+    """
+    def __call__(self):
+        form = self.request.form
+        if not 'pdf_name' in form:
+            # Should not happen.
+            self.errors.append('file_not_specified')
+            return self.index()
+
+        filename = form['pdf_name']
+
+        prefix = self.generate_filename_prefix()
+        if not filename.startswith(prefix):
+            # The user should not be able to see this file.
+            raise Unauthorized()
+
+        if not filename in os.listdir(self.tempdir):
+            self.errors.append('file_not_found')
+            self.request.response.setStatus(404)
+            return self.index()
+
+        self.pdf_file = file('%s/%s' % (self.tempdir, filename),
+                             'r')
+        self.request.response.setHeader("Content-type",
+                                        "application/pdf")
+        return self.pdf_file.read()
+
+class SendForm(BaseView):
+    """ If the user does not have Javascript enabled, he is
+    redirected to this page.
+    This page generates the PDF using the URL of the previous page
+    and shows a form to send it by mail.
+
+    It should logically show the same thing than the previous page
+    (the user has no JS enabled so no funky stuff happened).
+    The only case it could fail is if the previous page was processing a
+    POST form. In this case, we can just render the page as if it was
+    called with a simple GET request.
+    """
+
+    def get_page_source(self):
+        """ Returns the HTML source of a web page, considering
+        that the URL of the page is contained in the form under
+        the 'page_url' key.
+        """
+        url = self.request.form['page_url']
+        context_url = self.context.absolute_url()
+
+        view_name, get_params = extract_from_url(url, context_url)
+
+        # Now we will reinject the GET parameters in the request.
+        if get_params:
+            for key in get_params:
+                self.request.form[key] = get_params[key]
+
+        if not view_name:
+            ttool = getToolByName(self.context, 'portal_types')
+            if self.context.portal_type in ttool:
+                context_type = ttool[self.context.portal_type]
+                view_name = context_type.getProperty('immediate_view')
+
+        try:
+            view = self.context.restrictedTraverse(view_name)
+        except:
+            return
+
+        return view()
 
     def generate_temp_filename(self):
         """ Generates the filename used to store the PDF file.
@@ -141,9 +231,10 @@ class SendByMail(BrowserView):
         if not filename:
             self.errors.append('filename_generation_failed')
             return
-
+        
         file_path = '%s/%s' % (self.tempdir, filename)
         self.pdf_file = file(file_path, "wb")
+        self.filename = filename
 
         url = self.context.absolute_url()
         pdf = pisa.CreatePDF(
@@ -163,11 +254,59 @@ class SendByMail(BrowserView):
         """ Checks the form submitted when the user clicks on
         the 'send by mail' button.
         """
-        
+        form = self.request.form
+        fields = ['name',
+                  'name_recipient',
+                  'email',
+                  'email_recipient',
+                  'title',
+                  'content']
+
+        # All fields are mandatory
+        for field in fields:
+            if not form.get(field):
+                self.errors.append(field)
+
+        # We check that emails are real emails.
+        mail_validator = validation.validatorFor('isEmail')
+        for field in ['email', 'email_recipient']:
+            value = form.get(field)
+            if not value:
+                continue
+            if not mail_validator(value) == 1:
+                self.errors.append('invalid_' + field)
+
+    def get_values(self):
+        """ Provides the values used to fill the form.
+        """
+        if self.errors:
+            return self.request.form
+
+        values = {'filename': self.filename}
+        if self.get_user():
+            values['name'] = self.get_user_fullname()
+            values['email'] = self.get_user_email()
+
+        return values
 
     def process_form(self):
-        """ Process the submitted form.
         """
+        """
+        form = self.request.form
+
+        mfrom = '%s <%s>' % (form['name'],
+                             form['email'])
+        mto = '%s <%s>' % (form['name_recipient'],
+                           form['email_recipient'])
+
+        self.pdf_file = file('%s/%s' % (self.tempdir,
+                                        form['filename']),
+                             'r')
+        send_message(mfrom,
+                     mto,
+                     form['title'],
+                     form['content'],
+                     self.pdf_file)
 
     def __call__(self):
         form = self.request.form
@@ -200,7 +339,7 @@ class SendByMail(BrowserView):
         return self.index()
                 
 
-class SendByMailAjax(SendByMail):
+class SendFormAjax(SendForm):
     """ This class contains a set of methods that are called
     with jquery.pyproxy.
     """
---
+++ collective/sendaspdf/emailer.py
@@ -0,0 +1,149 @@
+################################################################################
+# Stollen from watcher list
+# http://svn.plone.org/svn/collective/collective.watcherlist/trunk/collective/watcherlist/
+################################################################################
+
+import logging
+from email.MIMEText import MIMEText
+from email.MIMEBase import MIMEBase
+from email.MIMEMultipart import MIMEMultipart
+from smtplib import SMTPException
+import pkg_resources
+import socket
+
+from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone.utils import getSiteEncoding
+from Products.CMFPlone.utils import safe_unicode
+from zope.app.component.hooks import getSite
+from zope.component import getMultiAdapter
+
+zope2_egg = pkg_resources.working_set.find(
+    pkg_resources.Requirement.parse('Zope2'))
+USE_SECURE_SEND = True
+if zope2_egg and (zope2_egg.parsed_version >=
+                  pkg_resources.parse_version('2.12.3')):
+    USE_SECURE_SEND = False
+
+logger = logging.getLogger('collective.sendaspdf')
+DEFAULT_CHARSET = 'utf-8'
+
+def get_charset():
+    """Character set to use for encoding the email.
+
+    If encoding fails we will try some other encodings.  We hope
+    to get utf-8 here always actually.
+
+    The getSiteEncoding call also works when portal is None, falling
+    back to utf-8.  But that is only on Plone 4, not Plone 3.  So we
+    handle that ourselves.
+    """
+    charset = None
+    portal = getSite()
+    if portal is None:
+        return DEFAULT_CHARSET
+    charset = portal.getProperty('email_charset', '')
+    if not charset:
+        charset = getSiteEncoding(portal)
+    return charset
+
+def su(value):
+    """Return safe unicode version of value.
+    """
+    return safe_unicode(value, encoding=get_charset())
+
+def get_mail_host():
+    """Get the MailHost object.
+
+    Return None in case of problems.
+    """
+    portal = getSite()
+    if portal is None:
+        return None
+    request = portal.REQUEST
+    ctrlOverview = getMultiAdapter((portal, request),
+                                   name='overview-controlpanel')
+    mail_settings_correct = not ctrlOverview.mailhost_warning()
+    if mail_settings_correct:
+        mail_host = getToolByName(portal, 'MailHost', None)
+        return mail_host
+
+
+def prepare_mail_message(msg, attachment):
+    """ Creates the message
+    """
+    html = msg
+
+    # First, we transform the message in a text format.
+    portal_transforms = getToolByName(getSite(),
+                                      'portal_transforms')
+    plain = portal_transforms.convert('html_to_text',
+                                      html).getData()
+
+    # We definitely want unicode at this point.
+    plain = su(plain)
+    html = su(html)
+
+    # We must choose the body charset manually.  Note that the
+    # goal and effect of this loop is to determine the
+    # body_charset.
+    for body_charset in 'US-ASCII', get_charset(), 'UTF-8':
+        try:
+            plain.encode(body_charset)
+            html.encode(body_charset)
+        except UnicodeEncodeError:
+            pass
+        else:
+            break
+
+    # Encoding should work now; let's replace errors just in case.
+    plain = plain.encode(body_charset, 'replace')
+    html = html.encode(body_charset, 'xmlcharrefreplace')
+
+    text_part = MIMEText(plain, 'plain', body_charset)
+    html_part = MIMEText(html, 'html', body_charset)
+
+    # Now the attachment.
+    attach = MIMEBase('application', 'pdf')
+    attach.set_payload(attachment.read())
+
+    # Okay, we send both plain text and html
+    email_msg = MIMEMultipart('alternative')
+    email_msg.epilogue = ''
+    email_msg.attach(text_part)
+    email_msg.attach(html_part)
+    email_msg.attach(attach)
+    return email_msg
+
+def send_message(mfrom, mto, subject, message, attachment):
+    """ 
+    """
+    message = prepare_mail_message(message, attachment)
+
+    mail_host = get_mail_host()
+    if mail_host is None:
+        return
+
+    mfrom = su(mfrom)
+    mto = su(mto)
+    subject = su(subject)
+    header_charset = get_charset()
+
+    try:
+        if USE_SECURE_SEND:
+            mail_host.secureSend(message=message,
+                                 mto=mto,
+                                 mfrom=mto,
+                                 subject=subject,
+                                 charset=header_charset)
+        else:
+            mail_host.send(message,
+                           mto=address,
+                           mfrom=mto,
+                           subject=subject,
+                           immediate=True,
+                           charset=header_charset)
+    except (socket.error, SMTPException):
+        logger.warn('Could not send email to %s with subject %s',
+                    address, subject)
+    except:
+        raise
